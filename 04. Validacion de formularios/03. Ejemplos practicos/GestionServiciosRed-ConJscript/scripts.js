25b8b680cd500634d935f461cf779a14760eabc6
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2022-07-04 17:29:37 -0300

    Add files via upload
    armada la función que valida los campos, reorganizado el código.

--- 04. Validacion de formularios/03. Ejemplos practicos/GestionServiciosRed-ConJscript/scripts.js
+++ 04. Validacion de formularios/03. Ejemplos practicos/GestionServiciosRed-ConJscript/scripts.js
@@ -17,17 +17,56 @@ const campos = {
 
 const validaciones = {
   "apellido" : /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
-  "nombres" : false,
-  "numero_documento" : false,
+  "nombres" : /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+  "numero_documento" : /^\d{7,8}$/,
   "domicilio": false,
   "piso" : false,
   "telefono" : false  
 }
 
-const validarCampo = (e) => {  
+
+const validarCampoFeedback = (evento) => {
+  let campoNombre = evento.target.name
+  let campoValor = evento.target.value;
+  let campoExpReg = validaciones[evento.target.name];
+  let campoAlertaAlUsuario = evento.target.title;
+  let CampoResultadoValidacion = campoExpReg.test(campoValor);
+
+  console.clear();
+  console.log("validando ", campoNombre, "\n", campoValor, "\n", campoExpReg, "\n", campoAlertaAlUsuario, "\n", CampoResultadoValidacion);
+
+  /*
+  Clases necesarias:
+  normal_input: un input antes de un evento keyup (sin error)
+  error_input: un input despues de keyup y no validado
+  show_msg: display: block para la label de abajo del input no validado
+  hide_msg: display: none para la label de abajo del input no validado 
+  Ademas es necesaria una label abajo del input a validar, para mostrar el mensaje.
+  */
+  if (CampoResultadoValidacion) {
+    console.log("validado!");
+    evento.target.classList.remove("error_input");
+    evento.target.classList.add("normal_input");
+    evento.target.nextElementSibling.classList.remove("show-msg");
+    evento.target.nextElementSibling.classList.add("hide-msg");
+
+  } else {
+    console.log("NO validado");
+    evento.target.classList.remove("normal_input");
+    evento.target.classList.add("error_input");    
+    evento.target.nextElementSibling.classList.remove("hide-msg");
+    evento.target.nextElementSibling.classList.add("show-msg");
+    evento.target.nextElementSibling.textContent = campoAlertaAlUsuario;
+  }
+
+}
+
+const validarCampos = (e) => {  
     //console.log(`Valido el input : ${e.target.name}`);  
     switch (e.target.name){
       case "apellido":
+        validarCampoFeedback(e);
+        /*
         let valor = e.target.value;
         let expReg = validaciones[e.target.name];
         let alertaAlUsuario = e.target.title;
@@ -38,8 +77,18 @@ const validarCampo = (e) => {
 
         if (resultadoValidacion) {
           console.log("validado!");
+          e.target.classList.remove("error_input");
+          e.target.classList.add("normal_input");
+          e.target.nextElementSibling.classList.remove("show-msg");
+          e.target.nextElementSibling.classList.add("hide-msg");
+
         } else {
           console.log("NO validado");
+          e.target.classList.remove("normal_input");
+          e.target.classList.add("error_input");
+          //console.log(e.target.nextElementSibling)
+          e.target.nextElementSibling.classList.remove("hide-msg");
+          e.target.nextElementSibling.classList.add("show-msg");
         }
         
         console.log("Validando el apellido con ", expReg);   
@@ -47,15 +96,21 @@ const validarCampo = (e) => {
         console.log("valor: ", valor);
         console.log(expReg);
         console.log("resultado validacion: ", resultadoValidacion);
-        
+        */
       break;  
       case "nombres":
+        validarCampoFeedback(e);
+        /*
         console.log("Validando los nombres con ", e.target.pattern);
         console.log("Mensaje al usuario: ", e.target.title);        
+        */
       break; 
       case "numero_documento":
+        validarCampoFeedback(e);
+        /*
         console.log("Validando el N° de documento con ", e.target.pattern);
         console.log("Mensaje al usuario: ", e.target.title);        
+        */
       break; 
       case "domicilio":
         console.log("Validando el domicilio con ", e.target.pattern);
@@ -74,7 +129,7 @@ const validarCampo = (e) => {
 
 $inputs.forEach(element => {
   //console.log(element.id)    
-  element.addEventListener('keyup', (e) => validarCampo(e));
+  element.addEventListener('keyup', (e) => validarCampos(e));
   //element.addEventListener('blur', (e) => validarCampo(e));
 });
 
